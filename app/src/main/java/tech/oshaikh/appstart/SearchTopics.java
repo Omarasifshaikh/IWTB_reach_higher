291e961653b986d6513469bc44b619ad413f07db
Omar Shaikh <omarasifshaikh@hotmail.com>
2015-11-21 14:59:27 -0800

    Initial commit

---
+++ app/src/main/java/tech/oshaikh/appstart/SearchTopics.java
@@ -0,0 +1,13 @@
+package tech.oshaikh.appstart;
+
+import android.os.Bundle;
+import tech.oshaikh.appstart.common.ActivityBase;
+
+public class SearchTopics extends ActivityBase {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_topics);
+    }
+}
