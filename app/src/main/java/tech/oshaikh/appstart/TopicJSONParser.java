be855d9d421ef02c2b44a03648e69ecc94b797f8
Omar Shaikh <omarasifshaikh@hotmail.com>
2015-11-21 17:12:54 -0800

    Working Topic autocomplete with simple JSON parsing

---
+++ app/src/main/java/tech/oshaikh/appstart/TopicJSONParser.java
@@ -0,0 +1,48 @@
+package tech.oshaikh.appstart;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import tech.oshaikh.appstart.model.Topic;
+
+/**
+ * Created by omar on 11/21/15.
+ */
+public class TopicJSONParser {
+    private static final String TAG = "appstart";
+    public static List<Topic> parseFile(String content){
+        try{
+            JSONObject o1 = new JSONObject(content);
+            String con1 = o1.getString("data");
+            JSONArray ar = new JSONArray(con1);
+            List<Topic> libraryEntryList = new ArrayList<>();
+            int length = ar.length();
+            int j=0;
+            for (int i = 0; i < length; i++) {
+                JSONObject obj = ar.getJSONObject(j);
+                Topic topic = new Topic();
+                topic.setId(Integer.parseInt(obj.getString("id").substring(1,obj.getString("id").length()-1)));
+                topic.setName(obj.getString("topic").substring(2,obj.getString("topic").length()-2));
+                if(!obj.isNull("id")) {
+                    libraryEntryList.add(topic);
+                    j++;
+                }
+                else{
+                    topic = null;
+                    i--;
+                    length--;
+                    j++;
+                }
+            }
+            return libraryEntryList;
+
+        }catch (JSONException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ app/src/main/java/tech/oshaikh/appstart/model/Topic.java
@@ -0,0 +1,33 @@
+package tech.oshaikh.appstart.model;
+
+/**
+ * Created by omar on 11/21/15.
+ */
+public class Topic {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Topic{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
